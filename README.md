d74b75916977e78816a545f08dac354725052470
Robert Ancell <robert.ancell@canonical.com>
2022-09-07 22:58:15 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Receive input from a MIDI device. Designed to work with the [HobbyTronics USB Host Board](https://www.hobbytronics.co.uk/usb-host-midi).
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+
+(The metadata above is needed for package search.)
---
+++ midi.ts
@@ -0,0 +1,60 @@
+//% weight=0 icon="\uf001" color="#5ea9dd"
+namespace midi {
+    var onNoteOff = null
+    var onNoteOn = null
+
+    /**
+    * setup MIDI
+    */
+    //% block="setup MIDI"
+    export function initMidi () {
+        serial.redirect(SerialPin.P0, SerialPin.P1, BaudRate.BaudRate31250)
+        basic.forever (function () {
+           let data = serial.readBuffer(1)
+           if (data.length == 0) {
+               return
+           }
+
+           /// Commands have first bit set, skip other values
+           let command = data[0]
+           if (command & 0b1000000 == 0) {
+               return
+           }
+           let type = (command & 0x0b01110000) >> 4
+           let channel = command & 0b00001111
+
+           switch (type) {
+           case 0:
+               data = serial.readBuffer(2)
+               if (onNoteOff != null) {
+                   onNoteOff(channel, data[0], data[1])
+               }
+               break
+           case 1:
+               data = serial.readBuffer(2)
+               if (onNoteOn != null) {
+                   onNoteOn(channel, data[0], data[1])
+               }
+               break
+           default:
+               return
+           }
+        })
+    }
+
+    /**
+    * Registers code to run when a note off MIDI event is received.
+    */
+    //% block="on note off"
+    export function onMidiNoteOffEvent (cb: (channel: number, key: number, velocity: number) => void) {
+        onNoteOff = cb
+    }
+
+    /**
+    * Registers code to run when a note on MIDI event is received.
+    */
+    //% block="on note on"
+    export function onMidiNoteOnEvent (cb: (channel: number, key: number, velocity: number) => void) {
+        onNoteOn = cb
+    }
+}
