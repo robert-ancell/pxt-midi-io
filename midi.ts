1316f5c02ce4671e3adc55d0e3cf2e5d8e86f7cb
Robert Ancell <robert.ancell@canonical.com>
2022-09-07 23:19:29 +1200

    Remove init function

--- midi.ts
+++ midi.ts
@@ -1,40 +1,34 @@
 //% weight=0 icon="\uf001" color="#5ea9dd" block="MIDI"
 namespace midi {
-    var onNoteOff = null
-    var onNoteOn = null
+    let initialized = false
 
-    /**
-    * setup MIDI
-    */
-    //% block="setup MIDI"
-    export function initMidi () {
+    let onNoteOffHandler: (channel: number, key: number, velocity: number) => void
+    let onNoteOnHandler: (channel: number, key: number, velocity: number) => void
+
+    function init () {
+        if (initialized) return
+        initialized = true
         serial.redirect(SerialPin.P0, SerialPin.P1, BaudRate.BaudRate31250)
         basic.forever (function () {
            let data = serial.readBuffer(1)
-           if (data.length == 0) {
-               return
-           }
+           if (data.length == 0) return
 
            /// Commands have first bit set, skip other values
            let command = data[0]
-           if (command & 0b1000000 == 0) {
-               return
-           }
+           if (command & 0b1000000 == 0) return
            let type = (command & 0x0b01110000) >> 4
            let channel = command & 0b00001111
 
            switch (type) {
            case 0:
                data = serial.readBuffer(2)
-               if (onNoteOff != null) {
-                   onNoteOff(channel, data[0], data[1])
-               }
+               if (onNoteOffHandler)
+                   onNoteOffHandler(channel, data[0], data[1])
                break
            case 1:
                data = serial.readBuffer(2)
-               if (onNoteOn != null) {
-                   onNoteOn(channel, data[0], data[1])
-               }
+               if (onNoteOnHandler)
+                   onNoteOnHandler(channel, data[0], data[1])
                break
            default:
                return
@@ -46,15 +40,17 @@ namespace midi {
     * Registers code to run when a note off MIDI event is received.
     */
     //% block="on note off"
-    export function onMidiNoteOffEvent (cb: (channel: number, key: number, velocity: number) => void) {
-        onNoteOff = cb
+    export function onNoteOff (cb: (channel: number, key: number, velocity: number) => void) {
+        init()
+        onNoteOffHandler = cb
     }
 
     /**
     * Registers code to run when a note on MIDI event is received.
     */
     //% block="on note on"
-    export function onMidiNoteOnEvent (cb: (channel: number, key: number, velocity: number) => void) {
-        onNoteOn = cb
+    export function onNoteOn (cb: (channel: number, key: number, velocity: number) => void) {
+        init()
+        onNoteOnHandler = cb
     }
 }
